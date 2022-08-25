ffb5ba1f5156739cd495f4d90c1225bcd3b97851
Joe Chen <mrjoechen@qq.com>
2022-08-25 16:07:36 +0800

    [update] login module

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -2,27 +2,27 @@ import { Request, Response, NextFunction } from 'express';
 import * as userService from '../user/user.service';
 import bcrypt from 'bcrypt';
 
-
-export const validateLoginData = async ( 
-    request: Request,
-    response: Response,
-    next: NextFunction
+export const validateLoginData = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
 ) => {
-    console.log('验证用户登录数据');
+  console.log('验证用户登录数据');
 
-    const { name, password } = request.body;
+  const { name, password } = request.body;
+  console.log(password);
 
-    if(!name) return next(new Error('NAME_IS_REQUIRED'));
+  if (!name) return next(new Error('NAME_IS_REQUIRED'));
 
-    if(!password) return next(new Error('PASSWORD_IS_REQUIRED'));
+  if (!password) return next(new Error('PASSWORD_IS_REQUIRED'));
 
-    const user = await userService.getUserByName(name, { password: true});
-    console.log(user)
-    if (!user) return next(new Error('USER_DOES_NOT_EXIST'));
+  const user = await userService.getUserByName(name, { password: true });
+  console.log(user);
+  if (!user) return next(new Error('USER_DOES_NOT_EXIST'));
 
-    const matched = await bcrypt.compare(password, user.password);
-    if(!matched) return next(new Error('PASSWORD_DOES_NOT_MATCH'));
+  const matched = await bcrypt.compare(password, user.password);
+  if (!matched) return next(new Error('PASSWORD_DOES_NOT_MATCH'));
 
-    console.log(user);
-    next();
-}
+  console.log(user);
+  next();
+};
--- src/user/user.middleware.ts
+++ src/user/user.middleware.ts
@@ -1,33 +1,32 @@
 import { Request, Response, NextFunction } from 'express';
 import * as userService from './user.service';
-import * as bcrypt from 'bcrypt'
+import * as bcrypt from 'bcrypt';
 
-export const validateUserData = async ( 
-    request: Request,
-    response: Response,
-    next: NextFunction
+export const validateUserData = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
 ) => {
+  const { name, password } = request.body;
+  console.log('验证用户数据');
 
-    const { name, password } = request.body;
-    console.log('验证用户数据');
+  if (!name) return next(new Error('NAME_IS_REQUIRED'));
 
-    if(!name) return next(new Error('NAME_IS_REQUIRED'));
+  if (!password) return next(new Error('PASSWORD_IS_REQUIRED'));
 
-    if(!password) return next(new Error('PASSWORD_IS_REQUIRED'));
+  const user = await userService.getUserByName(name);
+  console.log(user);
+  if (user) return next(new Error('USER_ALREADY_EXIST'));
+  next();
+};
 
-    const user = await userService.getUserByName(name);
-    if (!user) return next(new Error('USER_ALREADY_EXIST'));
-    console.log(user);
-    next();
-}
-
-export const hashPassword = async ( 
-    request: Request,
-    response: Response,
-    next: NextFunction
+export const hashPassword = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
 ) => {
-    const { password } = request.body;
+  const { password } = request.body;
 
-    request.body.password = await bcrypt.hash(password, 10);
-    next();
-} 
+  request.body.password = await bcrypt.hash(password, 10);
+  next();
+};
